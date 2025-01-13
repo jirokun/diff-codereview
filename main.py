fb97f1f65bbf36811285ff3d9155fe400472d644
Jiro Iwamoto <jirokun@gmail.com>
2025-01-13 15:24:12 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+
+import os
+from openai import OpenAI
+
+api_key = os.getenv("DEEPSEEK_API_KEY")
+
+client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "コードレビューをお願いします"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
